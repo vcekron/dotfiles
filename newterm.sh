f355f7c229278e6b99b1a88e88e504b84c5a324c
danikron <daniel.kronberg@gmail.com>
2016-05-22 02:21:13 +0200

    Tweaked sed argument

--- newterm.sh
+++ newterm.sh
@@ -1,4 +1,4 @@
 #! /bin/zsh
 
-dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //' | sed 's,\~.\?,'"$HOME"'/,')%\"}
+dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //; s,\~.\?,'"$HOME"'/,')%\"}
 urxvt -cd $dir
