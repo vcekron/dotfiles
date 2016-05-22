1f3bb99cd5080f4ab31f4a4bd5af528ef20e241d
danikron <daniel.kronberg@gmail.com>
2016-05-22 02:46:59 +0200

    Tweaked script so that it defaults to home if no path is found

--- newterm.sh
+++ newterm.sh
@@ -1,4 +1,11 @@
 #! /bin/zsh
 
 dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //; s,\~.\?,'"$HOME"'/,')%\"}
-urxvt -cd $dir
+
+if [[ $(echo $dir | head -c 1) == "/" ]]
+then
+	urxvt -cd $dir
+else
+	urxvt
+fi
+
