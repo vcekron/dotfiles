a034dddfb6a36fc9c9df7337e73cb738bc5a5dc1
danikron <daniel.kronberg@gmail.com>
2016-05-30 17:44:42 +0200

    Added functionality to make newterm work on Zathura, with or without tabbed

--- newterm.sh
+++ newterm.sh
@@ -1,6 +1,15 @@
 #! /bin/zsh
 
-dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //; s,\~.\?,'"$HOME"'/,')%\"}
+echo $(xprop -id $(xdotool getactivewindow) WM_CLASS) | read foo bar baz class
+
+if [[ $class == '"URxvt"' ]]
+then
+	dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //; s,\~.\?,'"$HOME"'/,')%\"}
+elif [[ $class == '"tabbed"' || $class == '"Zathura"' ]]
+then
+	echo $(xprop -id $(xdotool getactivewindow) WM_NAME) | read foo bar name
+	dir=${$(dirname $name)#\"}
+fi
 
 if [[ $(echo $dir | head -c 1) == "/" ]]
 then
