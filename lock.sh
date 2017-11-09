aa1be2f9b6f4639688f95dcdea46309cd21dafb2
danikron <daniel.kronberg@gmail.com>
2017-11-09 23:26:09 +0100

    Added external script for i3ock settings.

---
+++ lock.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+i3lock \
+	--insidecolor=1d1f21ff \
+	--ringcolor=c5c8c6ff \
+	--line-uses-inside \
+	--keyhlcolor=8c9440ff \
+	--bshlcolor=a54242ff \
+	--insidevercolor=1d1f21bb \
+	--ringvercolor=b264bbff \
+	--insidewrongcolor=a54242ff \
+	--ringwrongcolor=cc6666ff \
+	--timecolor=c5c8c6ff \
+	--datecolor=c5c8c6ff \
+	--textcolor=c5c8c6ff \
+	--timestr="%H:%M" \
+	--timesize=34 \
+	--datestr="%A, %d %b" \
+	--ring-width=2 \
+	--blur 5 \
+	--clock \
+	--indicator
