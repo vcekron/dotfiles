6dabd2a290da3579b8cacaeff27d0105112a4161
danikron <daniel.kronberg@gmail.com>
2018-09-28 18:05:28 +0200

    Indent fix

--- newterm.sh
+++ newterm.sh
@@ -2,17 +2,14 @@
 
 echo $(xprop -id $(xdotool getactivewindow) WM_CLASS) | read foo bar baz class
 
-if [[ $class == '"URxvt"' ]]
-then
+if [[ $class == '"URxvt"' ]] ; then
 	dir=${$(xprop -id $(xdotool getactivewindow) WM_ICON_NAME | sed 's/WM_ICON_NAME(STRING) = \".*: //; s,\~.\?,'"$HOME"'/,')%\"}
-elif [[ $class == '"tabbed"' || $class == '"Zathura"' ]]
-then
+elif [[ $class == '"tabbed"' || $class == '"Zathura"' ]] ; then
 	echo $(xprop -id $(xdotool getactivewindow) WM_NAME) | read foo bar name
 	dir=${$(dirname $name)#\"}
 fi
 
-if [[ $(echo $dir | head -c 1) == "/" ]]
-then
+if [[ $(echo $dir | head -c 1) == "/" ]] ; then
 	urxvt -cd $dir
 else
 	urxvt
