be2a8bfd0c558eb9caecf1fadcdce26b6c20127b
danikron <daniel.kronberg@gmail.com>
2018-10-01 12:44:19 +0200

    Rewrote external rules to use simplified conditionals

--- bspwm/external_rules.sh
+++ bspwm/external_rules.sh
@@ -10,12 +10,17 @@ instance=$3
 
 #Telegram
 
-if [ "$class" = TelegramDesktop ] ; then
-	title=$(xtitle "$wid")
-	if [ "$title" = "Media viewer" ] ; then
-		echo "state = fullscreen"
-	else
-		echo "state = pseudo_tiled"
-		echo "locked = on"
-	fi
-fi
+case $class in
+	"TelegramDesktop")
+		title=$(xtitle "$wid")
+		case $title in
+			"Media viewer")
+				echo "state = fullscreen"
+				;;
+			*)
+				echo "state = pseudo_tiled"
+				echo "locked = on"
+				;;
+		esac
+		;;
+esac
