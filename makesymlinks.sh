630dfd19658ce0bec26d9d1f254ee7cb4a70eca6
danikron <daniel.kronberg@gmail.com>
2018-10-26 11:42:41 +0200

    Removed auto-install of zsh

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
 
-########## Variables
+# Variables
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="gtkrc-2.0 makepkg.conf zlogin zprofile zshrc Xresources xinitrc Xmodmap"    # list of files/folders to symlink in homedir
 conf_files="bspwm gkt-3.0 rofi sxhkd zathura compton.conf redshift.conf"
 
-##########
+# Create symlinks
 
-# create dotfiles_old in homedir
+##Create dotfiles_old in homedir
 echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
 mkdir -p $olddir
 echo "done"
@@ -23,7 +19,7 @@ echo -n "Changing to the $dir directory ..."
 cd $dir
 echo "done"
 
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
+##Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
 for file in $files; do
 	echo "Moving any existing dotfiles from ~ to $olddir."
 	mv ~/.$file ~/$olddir
@@ -61,39 +57,3 @@ ln -s $dir/rc.conf ~/.config/ranger/
 
 echo "Creating symlink to startup script."
 ln -s $dir/startup /usr/local/bin/
-
-<<install
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-	# Clone my oh-my-zsh repository from GitHub only if it isn't already present
-	#if [[ ! -d $dir/oh-my-zsh/ ]]; then
-	#    git clone http://github.com/robbyrussell/oh-my-zsh.git
-	#fi
-	# Set the default shell to zsh if it isn't currently set to zsh
-	if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-		chsh -s $(which zsh)
-	fi
-else
-	# If zsh isn't installed, get the platform of the current machine
-	platform=$(uname);
-	# If the platform is Linux, try an apt-get to install zsh and then recurse
-	if [[ $platform == 'Linux' ]]; then
-		if [[ -f /etc/redhat-release ]]; then
-			sudo yum install zsh
-			install_zsh
-		fi
-		if [[ -f /etc/debian_version ]]; then
-			sudo apt-get install zsh
-			install_zsh
-		fi
-	# If the platform is OS X, tell the user to install zsh :)
-elif [[ $platform == 'Darwin' ]]; then
-		echo "Please install zsh, then re-run this script!"
-		exit
-	fi
-fi
-}
-
-install_zsh
-install
