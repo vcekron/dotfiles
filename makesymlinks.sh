6eeb01a945a45dd4acd9f261c8c5952128a3d42c
danikron <daniel.kronberg@gmail.com>
2018-10-26 14:29:18 +0200

    Rearranged config files and rewrote symlink script

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,59 +1,53 @@
-#!/bin/bash
+#!/bin/sh
 
 # Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gtkrc-2.0 makepkg.conf zlogin zprofile zshrc Xresources xinitrc Xmodmap"    # list of files/folders to symlink in homedir
-conf_files="bspwm gkt-3.0 rofi sxhkd zathura compton.conf redshift.conf"
+# dotfiles directory
+SOURCEDIR=~/dotfiles
+# old dotfiles backup directory
+OLDDIR=~/dotfiles_old
+# list of files/folders to symlink in homedir
+HOME_FILES=("gtkrc-2.0" "makepkg.conf" "xinitrc" "Xmodmap" "Xresources" "zlogin" "zprofile" "zshrc")
+CONF_DIRS=("gtk-3.0" "nvim/colors" "ranger" "rofi" "zathura")
+CONF_FILES=("bspwm" "compton.conf" "gkt-3.0/settings.ini" "nvim/colors/hybrid.vim" "nvim/colors/hybrid-light.vim" "nvim/init.vim" "ranger/rc.conf" "redshift.conf" "rofi/config" "sxhkd" "zathura/zathurarc")
 
 # Create symlinks
 
 ##Create dotfiles_old in homedir
-echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
-mkdir -p $olddir
+echo -n "Creating $OLDDIR for backup of any existing dotfiles in ~ ..."
+mkdir -p $OLDDIR
 echo "done"
 
 # change to the dotfiles directory
-echo -n "Changing to the $dir directory ..."
-cd $dir
+echo -n "Changing to the $SOURCEDIR directory ..."
+cd $SOURCEDIR
 echo "done"
 
-##Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $files
-for file in $files; do
-	echo "Moving any existing dotfiles from ~ to $olddir."
-	mv ~/.$file ~/$olddir
-	echo "Creating symlink to $file in home directory."
-	ln -s $dir/$file ~/.$file
+# Move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks from the homedir to any files in the ~/dotfiles directory specified in $HOME_FILES
+for file in $HOME_FILES; do
+	echo -n "Moving any existing dotfiles from ~ to $OLDDIR ..."
+	mv ~/.$file ~/$OLDDIR
+	echo "done"
+	echo -n "Creating symlink to $file in home directory ..."
+	ln -s $SOURCEDIR/$file ~/.$file
+	echo "done"
 done
 
-for file in $conf_files; do
-	echo "Moving any existing dotfiles from ~ to $olddir."
-	mv ~/.config/$file ~/$olddir
-	echo "Creating symlink to $file in ~/.config directory."
-	mkdir -p ~/.config
-	ln -s $dir/$file ~/.config/
+for dir in $CONF_DIRS; do
+	echo -n "Creating folders where needed ..."
+	mkdir -p ~/.config/$conf_dir
+	echo "done"
 done
 
-echo "Moving any existing dotfiles from ~/.config/nvim to $olddir."
-mv ~/.config/nvim/colors ~/$olddir/
-mv ~/.config/nvim/init.vim ~/$olddir
-echo "Creating symlink to init.vim and colors in ~/.config/nvim directory."
-mkdir -p ~/.config/nvim
-ln -s $dir/colors ~/.config/nvim/
-ln -s $dir/init.vim ~/.config/nvim/
+for file in $CONF_FILES; do
+	echo -n "Moving any existing dotfiles from ~ to $OLDDIR ..."
+	mv ~/.config/$file ~/$OLDDIR
+	echo "done"
+	echo -n "Creating symlink to $file in ~/.config directory ..."
+	ln -s $SOURCEDIR/$file ~/.config/
+	echo "done"
+done
 
-echo "Moving any existing mpv.conf from ~/.config/mpv to $olddir."
-mv ~/.config/mpv/mpv.conf ~/$olddir
-echo "Creating symlink to mpv.conf in ~/.config/mpv directory."
-mkdir -p ~/.config/mpv
-ln -s $dir/mpv.conf ~/.config/mpv/
-
-echo "Moving any existing rc.conf from ~/.config/ranger to $olddir."
-mv ~/.config/ranger/rc.conf ~/$olddir
-echo "Creating symlink to rc.conf in ~/.config/ranger directory."
-mkdir -p ~/.config/ranger
-ln -s $dir/rc.conf ~/.config/ranger/
-
-echo "Creating symlink to startup script."
-ln -s $dir/startup /usr/local/bin/
+echo -n "Creating symlink to startup script ..."
+ln -s $SOURCEDIR/startup /usr/local/bin/
+echo "done"
