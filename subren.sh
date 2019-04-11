6a8c4d6e1dcd93dc56099e5868c108ddde70eff0
danikron <danikron@pm.me>
2019-04-11 21:23:32 +0200

    Fixed broken renaming script

--- subren.sh
+++ subren.sh
@@ -2,20 +2,21 @@
 
 read -p "This will rename all the .srt-files in this folder to match the names of any .mkv-files in alphabetical order. Continue? [y/N] " -n 1 -r
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+echo #
+
+if [[ $REPLY =~ ^[Yy]$ ]]; then
 
 	LIST=$(ls | grep .mkv)
 	for file in *.srt; do
 		read line
-		mv -nv "${f}" "${line%.mkv}".srt | tee subren.log
+		mv -nv "${file}" "${line%.mkv}".srt | tee subren.log
 	done <<< "$LIST"
 
 	echo "done"
 
 	read -p "Keep log-file? [Y/n] " -n 1 -r
 
-	if [[$REPLY =~ ^[Nn]$ ]]
+	if [[$REPLY =~ ^[Nn]$ ]]; then
 		rm subren.log
 	fi
 
