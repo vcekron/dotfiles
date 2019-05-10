88cfeedf230d3f3777750f95b10b42c18bbf0931
danikron <danikron@pm.me>
2019-05-10 12:02:01 +0200

    Added a couple of scripts that facilitate my laziness

---
+++ flactomp3.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+FILES=( "$@" )
+FLACS=$( printf '%s\n' "${FILES[@]}" | grep '.flac$')
+UNKONWNS=$( printf '%s\n' "${FILES[@]}" | grep -v '.flac$')
+
+for flac in $FLACS; do
+	< /dev/null ffmpeg -i "$flac" -qscale:a 0 "${flac[@]/%flac/mp3}"
+done
+
+if [[ $UNKONWNS ]]; then
+	echo -e "\nUnknown files:"
+	for unknown in $UNKONWNS; do
+		echo -e "$unknown"
+	done
+fi
