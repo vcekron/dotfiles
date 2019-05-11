eda1b81433d92f90814ca1052c8e25069c47825c
danikron <daniel.kronberg@gmail.com>
2019-05-11 23:52:27 +0200

    Set battery warning to not try to use zenity if it's not installed

--- sysscripts/battwarn.sh
+++ sysscripts/battwarn.sh
@@ -4,7 +4,7 @@
 
 if [[ COUNT -ge 300 ]]; then
 	COUNT=0
-	zenity --warning --text "<span font_size='large' font_weight='bold'>Low battery</span>\n$BATT% of battery remaining" --title "Low battery warning" --width=150
+	[[ $(pacman -Qi zenity) ]] && zenity --warning --text "<span font_size='large' font_weight='bold'>Low battery</span>\n$BATT% of battery remaining" --title "Low battery warning" --width=150 &> /dev/null
 fi
 
 ((COUNT++))
