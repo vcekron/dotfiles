05ac9c3fdbc96841e310f0e67e6eda2a49ad8190
danikron <danikron@pm.me>
2019-05-13 08:59:03 +0200

    Changed script to be less stupid

--- sysscripts/restartpanel.sh
+++ sysscripts/restartpanel.sh
@@ -1,10 +1,6 @@
 #! /bin/sh
 
-xdo kill $(xdo id -m -a "$PANEL_WM_NAME" | head -n 1)
+id=$(xdo id -a "$PANEL_WM_NAME" | head -n 1)
+[[ $id ]] && xdo kill $id
 
-./.config/bspwm/panel &!
-
-wids=$(xdo id -m -a "$PANEL_WM_NAME")
-for wid in $wids; do
-	xdo above -t "$(xdo id -N Bspwm -n root | sort | head -n 1)" "$wid"
-done
+~/.config/bspwm/panel &!
