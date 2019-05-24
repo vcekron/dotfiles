9696ca8149e45c203231e5516b647b9afaf27d1c
danikron <danikron@pm.me>
2019-05-24 17:15:33 +0200

    Made vpn_state a bit more informative to avoid use of separate script for glances

--- sysscripts/vpncheck.sh
+++ sysscripts/vpncheck.sh
@@ -1,39 +1,39 @@
 #! /bin/sh
 
-VPN_IF=azirevpn
+VPN_IF=azirevpn-se1
 VPN_CHECK="curl -sS https://api.azirevpn.com/v1/check --max-time 2"
 VPN_CHECK_FILTER="grep '\"connected\": true'"
 POLL_INTERVAL=15
-POLL_DELAY_TICK=0
+poll_delay_tick=0
 
 while true; do
 
-	VPN_UP=$(ip r | grep ${VPN_IF})
+	vpn_up=$(ip r | grep ${VPN_IF})
 
-	if [[ $VPN_UP && $POLL_DELAY_TICK == 0 ]]; then
-		VPN_CHECK_RESULT=$(eval ${VPN_CHECK} 2> /tmp/vpn_err)
-		VPN_CONNECTED=$(echo "${VPN_CHECK_RESULT}" | eval ${VPN_CHECK_FILTER})
+	if [[ $vpn_up && $poll_delay_tick == 0 ]]; then
+		vpn_check_result=$(eval ${VPN_CHECK} 2> /tmp/vpn_err)
+		vpn_connected=$(echo "${vpn_check_result}" | eval ${VPN_CHECK_FILTER})
 		#echo "Check ran"
 	fi
 
-	if [[ $VPN_UP && $VPN_CONNECTED ]]; then
-		VPN_STATE="Connected"
-		POLL_DELAY_TICK=$(((POLL_DELAY_TICK + 1) % POLL_INTERVAL))
-	elif [[ $VPN_UP ]] ; then
-		VPN_STATE="Error"
-		POLL_DELAY_TICK=0
+	if [[ $vpn_up && $vpn_connected ]]; then
+		vpn_state="Connected"
+		poll_delay_tick=$(((poll_delay_tick + 1) % POLL_INTERVAL))
+	elif [[ $vpn_up ]] ; then
+		vpn_state="Error"
+		poll_delay_tick=0
 	else
-		VPN_STATE="Disconnected"
-		unset VPN_CHECK_RESULT
-		POLL_DELAY_TICK=0
+		vpn_state="Disconnected"
+		unset vpn_check_result
+		poll_delay_tick=0
 	fi
 
-	echo "$VPN_STATE" > /tmp/vpn_state
+	echo "$VPN_IF: $vpn_state" > /tmp/vpn_state
 
 	#Debug
-	#echo "$VPN_STATE"
-	#echo "$POLL_DELAY_TICK"
-	#echo "$VPN_CHECK_RESULT"
+	#echo "$vpn_state"
+	#echo "$poll_delay_tick"
+	#echo "$vpn_check_result"
 
 	sleep 1
 
--- sysscripts/vpnstate.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/sh
-
-echo -n "$(ip link | grep azirevpn | cut -d' ' -f2) "
-cat /tmp/vpn_state /tmp/vpn_err
