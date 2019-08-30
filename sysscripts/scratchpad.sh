dae9187480a38f0fe05bf38fe04bbb0f8d3fc69c
simokron <Simon.Kronberg@gmail.com>
2019-08-30 22:09:09 +0200

    Removed offset and fixed scratchpad for 1200p.

--- sysscripts/scratchpad.sh
+++ sysscripts/scratchpad.sh
@@ -25,7 +25,7 @@ RES=$(xrandr | grep primary | cut -d " " -f 4)
 
 if [[ $RES == '2560x1440+0+0' ]] ; then
 	urxvt -name scratchpad -g 132x26+650-0 -e $SCRIPTPATH/scratchid.sh &
-elif [[ $RES == '1920x1080+0+0' ]] ; then
+elif [[ $RES == '1920x1200+0+0' ]] ; then
 	urxvt -name scratchpad -g 124x26+326-0 -e $SCRIPTPATH/scratchid.sh &
 elif [[ $RES == '1920x1080+0+0' ]] ; then
 	urxvt -name scratchpad -g 124x26+326-0 -e $SCRIPTPATH/scratchid.sh &
