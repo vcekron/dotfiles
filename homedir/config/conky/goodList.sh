89558953bb191bf5ab2395d86778157f4d78bac3
scekron <Simon.Kronberg@gmail.com>
2019-11-26 17:08:14 +0100

    Massively improved the readability of `goodList.sh` & added `rConky` alias.

--- homedir/config/conky/goodList.sh
+++ homedir/config/conky/goodList.sh
@@ -5,26 +5,26 @@ else
 	offsetLeft=75; offsetRight=75
 fi
 
-arrayEvents=(); arrayDates=()
-arrayEvents+=('* Christmas Eve ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2019-12-24 00:00:00"}')
-#arrayEvents+=('* Christmas Eve'); arrayDates=('2019-12-24 00:00:00')
-arrayEvents+=('* New Year'"'"'s Eve ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2019-12-31 00:00:00"}')
-arrayEvents+=('* Silvia Neuchâtel ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2020-01-10 00:00:00"}')
-arrayEvents+=('* Prognosis Festival ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2020-03-20 00:00:00"}')
-arrayEvents+=('* Silvia Belgrade ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2020-04-10 00:00:00"}')
-#arrayEvents+=('* Silvia Genzano ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2020-06-26 00:00:00"}')
-arrayEvents+=('* Dynamo Metalfest ${alignr}${offset -'${offsetRight}'}${texeci 60 ~/.config/conky/timeToEvent.sh "2020-06-17 00:00:00"}')
-
-#for i in $(seq ${#arrayDates[@]}); do
-#	~/.config/conky/timeToEvent.sh ${arrayDates[$i-1]}
-#	${arrayDates[$i-1]}="~/.config/conky/timeToEvent.sh ${arrayDates[$i-1]}"
-#done
+arrayEvents=(); 							arrayDates=()
+arrayEvents+=('* Christmas Eve'); 			arrayDates+=('2019-12-24 00:00:00')
+arrayEvents+=('* New Year'"'"'s Eve'); 		arrayDates+=('2019-12-31 00:00:00')
+arrayEvents+=('* Silvia Neuchâtel'); 		arrayDates+=('2020-01-10 00:00:00')
+arrayEvents+=('* Prognosis Festival'); 		arrayDates+=('2020-03-20 00:00:00')
+arrayEvents+=('* Silvia Belgrade'); 		arrayDates+=('2020-04-10 00:00:00')
+#arrayEvents+=('* Silvia Genzano'); 			arrayDates+=('2020-06-26 00:00:00')
+arrayEvents+=('* Dynamo Metalfest'); 		arrayDates+=('2020-06-17 00:00:00')
 
 for i in $(seq ${#arrayEvents[@]}); do
+	# Convert the dates into time-until-event.
+	arrayDates[$i-1]=$(~/.config/conky/timeToEvent.sh ${arrayDates[$i-1]})
+
+	# Decide the correct color (${color}$ and ${color2}$ for odd and even rows, respectively).
 	if (( $i % 2 )); then
-		echo '${color}${offset '${offsetLeft}'}'${arrayEvents[$i-1]}
-		#'${alignr}${offset -'${offsetRight}'}'${arrayDates[$i-1]}
+		color='${color}'
 	else
-		echo '${color2}${offset '${offsetLeft}'}'${arrayEvents[$i-1]}
+		color='${color2}'
 	fi
+	
+	# Echo the final string.
+	echo ${color}'${offset '${offsetLeft}'}'${arrayEvents[$i-1]}'${alignr}${offset -'${offsetRight}'}'${arrayDates[$i-1]}
 done
