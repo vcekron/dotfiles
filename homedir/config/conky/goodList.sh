47c608babf71d3fb225a3c3430c902ed017e38ec
scekron <Simon.Kronberg@gmail.com>
2019-11-26 23:13:11 +0100

    Further improved usability and readability of `goodList.sh`.

--- homedir/config/conky/goodList.sh
+++ homedir/config/conky/goodList.sh
@@ -5,14 +5,16 @@ else
 	offsetLeft=75; offsetRight=75
 fi
 
-arrayEvents=(); 							arrayDates=()
-arrayEvents+=('* Christmas Eve'); 			arrayDates+=('2019-12-24 00:00:00')
-arrayEvents+=('* New Year'"'"'s Eve'); 		arrayDates+=('2019-12-31 00:00:00')
-arrayEvents+=('* Silvia Neuchâtel'); 		arrayDates+=('2020-01-10 00:00:00')
-arrayEvents+=('* Prognosis Festival'); 		arrayDates+=('2020-03-20 00:00:00')
-arrayEvents+=('* Silvia Belgrade'); 		arrayDates+=('2020-04-10 00:00:00')
-#arrayEvents+=('* Silvia Genzano'); 			arrayDates+=('2020-06-26 00:00:00')
-arrayEvents+=('* Dynamo Metalfest'); 		arrayDates+=('2020-06-17 00:00:00')
+arrayEvents=(); 								arrayDates=()
+arrayEvents+=('Christmas Eve'); 			arrayDates+=('2019-12-24 00:00:00')
+arrayEvents+=('New Year'"'"'s Eve');		arrayDates+=('2019-12-31 00:00:00')
+arrayEvents+=('Silvia Neuchâtel'); 			arrayDates+=('2020-01-10 00:00:00')
+arrayEvents+=('Prognosis Festival'); 		arrayDates+=('2020-03-20 00:00:00')
+arrayEvents+=('Silvia Belgrade'); 			arrayDates+=('2020-04-10 00:00:00')
+arrayEvents+=('Sweden Rock Festival'); 		arrayDates+=('2020-06-03 00:00:00')
+arrayEvents+=('Dynamo Metalfest'); 			arrayDates+=('2020-06-17 00:00:00')
+#arrayEvents+=('Silvia Genzano');  			arrayDates+=('2020-06-26 00:00:00')
+arrayEvents+=('South Korea');	 			arrayDates+=('2020-08-01 00:00:00')
 
 for i in $(seq ${#arrayEvents[@]}); do
 	# Convert the dates into time-until-event.
@@ -26,5 +28,5 @@ for i in $(seq ${#arrayEvents[@]}); do
 	fi
 	
 	# Echo the final string.
-	echo ${color}'${offset '${offsetLeft}'}'${arrayEvents[$i-1]}'${alignr}${offset -'${offsetRight}'}'${arrayDates[$i-1]}
+	echo ${color}'${offset '${offsetLeft}'}* '${arrayEvents[$i-1]}'${alignr}${offset -'${offsetRight}'}'${arrayDates[$i-1]}
 done
