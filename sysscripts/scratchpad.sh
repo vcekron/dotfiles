f3fbb331d8609a3ff887e618671fce0fa59426e8
scekron <Simon.Kronberg@gmail.com>
2019-12-09 12:13:08 +0100

    Fixed the geometry.
    Reverted to checking the full geometry for obvious reasons..

--- sysscripts/scratchpad.sh
+++ sysscripts/scratchpad.sh
@@ -22,26 +22,29 @@ if [[ $OLD ]] ; then
 fi
 
 if [[ "$HOSTNAME" == fjorgyn ]] ; then
-	case $(bspc query -M | wc -l) in
+	case $(xrandr | grep " connected " | wc -l) in
 		2)
-			RES=$(xrandr | grep HDMI-1 | cut -d " " -f 3 | cut -d + -f 1)
+			RES=$(xrandr | grep HDMI-1 | cut -d " " -f 3)
 			;;
 		*)
-			RES=$(xrandr | grep primary | cut -d " " -f 4 | cut -d + -f 1)
+			RES=$(xrandr | grep primary | cut -d " " -f 4)
 			;;
 	esac
 else
-	RES=$(xrandr | grep primary | cut -d " " -f 4 | cut -d + -f 1)
+	RES=$(xrandr | grep primary | cut -d " " -f 4)
 fi
 
 case $RES in
-	'2560x1440')
+	'2560x1440+0+0')
 		urxvt -name scratchpad -g 132x26+650-0 -e $SCRIPTPATH/scratchid.sh &
 		;;
-	'1920x1200')
+	'1920x1200+1920+0')
+		urxvt -name scratchpad -g 124x29+326-120 -e $SCRIPTPATH/scratchid.sh &
+		;;
+	'1920x1200+0+0')
 		urxvt -name scratchpad -g 124x29+326-0 -e $SCRIPTPATH/scratchid.sh &
 		;;
-	'1920x1080')
+	'1920x1080+0+0')
 		urxvt -name scratchpad -g 124x26+326-0 -e $SCRIPTPATH/scratchid.sh &
 		;;
 esac
