ceba6fa3d694b8101c737ef93e5623bf46edb28d
scekron <Simon.Kronberg@gmail.com>
2020-01-06 11:20:13 +0100

    Updated events list.

--- homedir/config/conky/goodList.sh
+++ homedir/config/conky/goodList.sh
@@ -18,14 +18,14 @@ fi
 
 # Create and fill an array with the events, and one with the corresponding dates.
 arrayEvents=(); 							arrayDates=()
-arrayEvents+=('Christmas Eve'); 			arrayDates+=('2019-12-24 00:00:00')
-arrayEvents+=('New Year'"'"'s Eve');		arrayDates+=('2019-12-31 00:00:00')
-arrayEvents+=('Silvia Neuchâtel'); 			arrayDates+=('2020-01-10 00:00:00')
+#arrayEvents+=('Christmas Eve'); 			arrayDates+=('2019-12-24 00:00:00')
+#arrayEvents+=('New Year'"'"'s Eve');		arrayDates+=('2019-12-31 00:00:00')
+arrayEvents+=('Silvia Neuchâtel'); 			arrayDates+=('2020-01-09 17:00:00')
+arrayEvents+=('Joanna, Sanna & Felicia');	arrayDates+=('2020-03-06 17:00:00')
 arrayEvents+=('Prognosis Festival'); 		arrayDates+=('2020-03-20 00:00:00')
 arrayEvents+=('Silvia Belgrade'); 			arrayDates+=('2020-04-10 00:00:00')
 arrayEvents+=('Dynamo Metalfest'); 			arrayDates+=('2020-06-17 00:00:00')
-arrayEvents+=('Kendrick Lamar Rome'); 			arrayDates+=('2020-07-07 00:00:00')
-#arrayEvents+=('Silvia Genzano');  			arrayDates+=('2020-06-26 00:00:00')
+arrayEvents+=('Kendrick Lamar Rome'); 		arrayDates+=('2020-07-07 00:00:00')
 arrayEvents+=('South Korea');	 			arrayDates+=('2020-08-01 00:00:00')
 arrayEvents+=('Nightwish Düsseldorf'); 		arrayDates+=('2020-11-21 00:00:00')
 
