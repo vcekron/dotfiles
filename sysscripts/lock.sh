2a1858ec25350b4cd2fedbdbc0afb3c0dc6a0876
scekron <Simon.Kronberg@gmail.com>
2020-05-27 16:41:21 +0200

    Made the lockscreen a lot glitchier-looking.

--- sysscripts/lock.sh
+++ sysscripts/lock.sh
@@ -4,56 +4,80 @@
 # ┇━┛┇━┛┇ ┇ ┗━┛┇ ┻┇━┛┛━┛┗━┛┇ ┛
 #
 # author: xero <x@xero.nu> http://xero.nu
+# modified by: scekron
 # requires: i3lock-color, imagemagick, maim
 
-maim -u /tmp/lock.png
-convert /tmp/lock.png /tmp/lock.jpg
-file=/tmp/lock.jpg
+maim -u /tmp/lock.png # Take a screenshot.
+convert -quality 100 /tmp/lock.png /tmp/lock.jpg # Convert it to jpg for the mapulations.
+file=/tmp/lock.jpg # This is the temporary file we will manipulate.
 
+# Create a datamosh function, which will perturb the data.
 function datamosh() {
-	fileSize=$(wc -c < "$file")
-	headerSize=1000
-	skip=$(shuf -i "$headerSize"-"$fileSize" -n 1)
-	count=$(shuf -i 1-10 -n 1)
-	for i in $(seq 1 $count);do byteStr=$byteStr'\x'$(shuf -i 0-255 -n 1); done;
-	printf $byteStr | dd of="$file" bs=1 seek=$skip count=$count conv=notrunc >/dev/null 2>&1
+	fileSize=$(wc -c < "$file") # File size in bytes..
+	headerSize=1000 # How many bytes before we start messing with the data.
+	skip=$(shuf -i "$headerSize"-"$fileSize" -n 1) # The start of the byte to mess with.
+	count=$(shuf -i 1-10 -n 1) # The number of bytes to manipulate.
+	# Loop over the number of bytes and generate a byte string.
+	for i in $(seq 1 $count); do
+		byteStr=$byteStr'\x'$(shuf -i 0-255 -n 1)
+	done
+	printf $byteStr | dd of="$file" bs=1 seek=$skip count=$count conv=notrunc >/dev/null 2>&1 # Copy the byte string into the file, overriding the previous data.
 }
 
-steps=$(shuf -i 40-70 -n 1)
-for i in $(seq 1 $steps);do datamosh "$file"; done
+steps=$(shuf -i 30-50 -n 1) # The number of datamosh applications.
+for i in $(seq 1 $steps); do
 
-GLITCHICON=${GLITCHICON:=""}
-PARAM=(--bar-indicator --bar-position h --bar-direction 1 --redraw-thread -t "" \
-	--bar-step 50 --bar-width 250 --bar-base-width 50 --bar-max-height 100 --bar-periodic-step 50 \
-	--bar-color 00000077 --keyhlcolor 00666666 --ringvercolor cc87875f --wrongcolor ffff0000 \
-	--veriftext="" --wrongtext="" --noinputtext="" )
-
-LOCK=()
-while read LINE
-do
-	if [[ "$LINE" =~ ([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+) ]]; then
-		W=${BASH_REMATCH[1]}
-		H=${BASH_REMATCH[2]}
-		Xoff=${BASH_REMATCH[3]}
-		Yoff=${BASH_REMATCH[4]}
-		if [ ! -z "$GLITCHICON" ]; then
-			IW=`identify -ping -format '%w' $GLITCHICON`
-			IH=`identify -ping -format '%h' $GLITCHICON`
-			MIDXi=$(($W / 2 + $Xoff - $IW / 2))
-			MIDYi=$(($H / 2 + $Yoff - $IH / 2))
-			LOCK+=($GLITCHICON -geometry +$MIDXi+$MIDYi -composite)
-		fi
+	# Rotate the image 90 degrees with a certain probability before messing with it.
+	tempVar=$(shuf -i 0-100 -n 1)
+	if [[ $tempVar -lt 11 ]]; then
+		convert -rotate 90 -quality 100 $file $file >/dev/null 2>&1
+		(( k++ ))
 	fi
-done <<<"$(xrandr)"
 
-convert /tmp/lock.jpg /tmp/lock.png >/dev/null 2>&1
+	datamosh "$file" # Just fuck my shit up.
+done
+
+# Rotate the image to the correct orientation again.
+remainingDegrees=$(( k*90 % 360 ))
+convert -rotate -$remainingDegrees -quality 100 /tmp/lock.jpg /tmp/lock.jpg >/dev/null 2>&1
+
+# The paramaters for i3lock-color.
+PARAM=(
+	--insidecolor=1d1f21ff \
+	--ringcolor=c5c8c6ff \
+	--line-uses-inside \
+	--keyhlcolor=8c9440ff \
+	--bshlcolor=a54242ff \
+	--separatorcolor=c5c8c6ff \
+	--insidevercolor=1d1f21ff \
+	--ringvercolor=b264bbff \
+	--insidewrongcolor=a54242ff \
+	--ringwrongcolor=a54242ff \
+	--timecolor=c5c8c6ff \
+	--datecolor=c5c8c6ff \
+	--timestr="%H:%M" \
+	--time-font="Source Sans Pro" \
+	--timesize=38 \
+	--datestr="%A %d %b" \
+	--date-font="Source Sans Pro" \
+	--datesize=16 \
+	--veriftext="" \
+	--wrong-font="Source Sans Pro" \
+	--ring-width=4 \
+	--clock \
+	--indicator \
+	--veriftext="" --wrongtext="" --noinputtext=""
+)
+
+# Convert our fucked up jpg back to png and remove temp files.
+convert -quality 100 /tmp/lock.jpg /tmp/lock.png >/dev/null 2>&1
 rm /tmp/lock.jpg
 file=/tmp/lock.png
 
-convert "$file" "${LOCK[@]}" "$file"
-
+# Lock the device.
 i3lock -n "${PARAM[@]}" -i "$file" > /dev/null 2>&1 &
 
+# If requested, also put the device to sleep.
 if [[ $1 == s ]]; then
 	sleep 1
 	systemctl suspend
