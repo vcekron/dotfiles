dd91f52462335b383aaaee763678dd3d007c4850
scekron <Simon.Kronberg@gmail.com>
2021-01-10 11:11:57 +0100

    Added step to disable the headset RGB

--- sysscripts/fetchheadsetbattery.sh
+++ sysscripts/fetchheadsetbattery.sh
@@ -5,7 +5,9 @@ headsetcontrol -cb | read -r BATTH
 if [[ $BATTH == "-2" ]] ; then
 	notify-send "Headset is disconnected" -t 5000 -i /usr/share/icons/Adwaita/48x48/devices/audio-headset.png
 elif [[ $BATTH == "-1" ]] ; then
+	headsetcontrol -l 0
 	notify-send "Headset is charging" -t 5000 -i /usr/share/icons/Adwaita/48x48/devices/audio-headset.png
 else
+	headsetcontrol -l 0
 	notify-send "Battery level: $BATTH%" -t 5000 -i /usr/share/icons/Adwaita/48x48/devices/audio-headset.png
 fi
