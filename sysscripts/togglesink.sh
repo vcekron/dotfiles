555d62f600c6a1682227d108dee5a0cb33239185
scekron <Simon.Kronberg@gmail.com>
2021-02-18 13:33:36 +0100

    Revert "Switched sink to new headset"
    This reverts commit 1e4fad4aaa4bf99b8482b271ac1653925e85dffc.

--- sysscripts/togglesink.sh
+++ sysscripts/togglesink.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
-#HEADPHONES="alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"
-HEADPHONES="alsa_output.usb-Corsair_CORSAIR_VIRTUOSO_SE_Wireless_Gaming_Headset_15ad45cd000700dc-00.iec958-stereo"
+HEADPHONES="alsa_output.usb-Corsair_CORSAIR_VOID_ELITE_Wireless_Gaming_Dongle-00.analog-stereo"
 case $(hostname) in
 	"freja")
 		SPEAKERS="alsa_output.pci-0000_08_00.4.iec958-stereo"
@@ -11,8 +10,7 @@ case $(hostname) in
 		;;
 esac
 
-#pacmd info | grep -e "Default sink.*${SPEAKERS}" && pacmd set-default-sink ${HEADPHONES} && headsetcontrol -l 0 || pacmd set-default-sink ${SPEAKERS}
-pacmd info | grep -e "Default sink.*${SPEAKERS}" && pacmd set-default-sink ${HEADPHONES} || pacmd set-default-sink ${SPEAKERS}
+pacmd info | grep -e "Default sink.*${SPEAKERS}" && pacmd set-default-sink ${HEADPHONES} && headsetcontrol -l 0 || pacmd set-default-sink ${SPEAKERS}
 
 ~/.config/bspwm/panel_resources/volume > "$PANEL_FIFO"
 
